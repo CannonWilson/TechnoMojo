433c701bcd0e79ae34a01e23fda17811737e51c2
CannonWilson <CannonGWilson@gmail.com>
2022-06-03 15:56:11 -0700

    Beginning to redo React content and break lessons into different files

---
+++ src/data/09 - React/ReactLessons.js
@@ -0,0 +1,333 @@
+module.exports = {
+	moduleName: '09 - React',
+	lessons: [
+		{
+			lessonName: 'Intro to React',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'Which is the correct command to create a new React app called my-app?',
+					answerChoices: [
+						"npx create react app my-app",
+						"npm install react",
+						"npx create-react-app my-app",
+						"npm create react app my-app"
+					],
+					correctAnswerIndex: 2
+				},
+				{
+					question: 'Which is the correct command to start the React local development server?',
+					answerChoices: [
+						"npm run build",
+						"npm start",
+						"npm server",
+						"npx build"
+					],
+					correctAnswerIndex: 1
+				},
+				{
+					question: 'What is a reusable bit of code that returns React elements?',
+					answerChoices: [
+						"Root",
+						"Class",
+						"View",
+						"Component"
+					],
+					correctAnswerIndex: 3
+				}
+			]	
+		},
+		{
+			lessonName: 'Props',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'More Props',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'useState',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'useState with Props',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'useEffect',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'useEffect with Dependencies',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		},
+		{
+			lessonName: 'React Router',
+			lessonDescription: "Default lesson description", 
+			exerciseDescription: "Default exercise description text",
+			submissionDescription: "Paste the entire contents of index.html that you wrote in the above sandbox into the textarea below. You have unlimited submissions, and only your latest submission will be recorded.",
+			introVideoUrl: "https://player.vimeo.com/video/698804582?h=b3f29101c3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			codeSandBoxUrl: "https://codesandbox.io/embed/brave-archimedes-uo2ucj?fontsize=14&hidenavigation=1&theme=dark",
+			answerVideoUrl: "https://player.vimeo.com/video/698856937?h=892db1de3b&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+			quiz: [
+				{
+					question: 'The first question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The second question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				},
+				{
+					question: 'The third question: ',
+					answerChoices: [
+						"First answer choice",
+						"Second answer choice",
+						"Third answer choice",
+						"Fourth answer choice"
+					],
+					correctAnswerIndex: 0
+				}
+			]	
+		}
+	]
+}
