0656b7f7e1c2323387f4572fff74b42fff4d723c
CannonWilson <CannonGWilson@gmail.com>
2022-06-29 15:33:17 -0700

    Created first basic tests for rendering App and Overview

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import {MemoryRouter} from 'react-router-dom'
+import { render, unmountComponentAtNode } from "react-dom"
+import { act } from "react-dom/test-utils"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from "./App.js"
+import Overview from './views/overview/Overview.js'
+
+
+let testUsername = 'KincannonW'
+let testPswd = process.env.REACT_APP_TEST_USER_PASSWORD
+
+let container = null
+let root = null
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div")
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  container.remove()
+  container = null
+})
+
+
+describe('test rendering', () => {
+  
+  it("renders App", () => {
+    act(() => {
+      ReactDOM.createRoot(container).render(<App />)    
+    })
+    expect(container.textContent).toContain('TechnoMojo')
+  })
+   
+  it("renders Overview", () => {
+    act(() => {
+      ReactDOM.createRoot(container).render(
+        <MemoryRouter>
+          <Overview />
+        </MemoryRouter>)
+    })
+    expect(container.textContent).toContain('Overall completion')
+  }) 
+  
+})
+
